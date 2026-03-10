bc56de613ae01f26a8c7e02e3b70261aaaa5c403
Danya Putilin <danyaputilin@yandex.ru>
2026-03-10 21:08:18 +0300

    project structure
    added __all_models.py, db_session.py, base.html, index.html ...

--- server.py
+++ server.py
@@ -1,16 +1,24 @@
 from flask import Flask
+from flask import render_template
+
+# Быза данных
+from backend import db_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dysnet_secret_key'
 
 
 app.route("/")
 app.route("/index")
 def index():
-    return ""
+    return render_template("index.html")
 
-
+    
 def main():
-    app.run(host="$HOST$", port="$PORT$")
+    """ Главная функция """
+
+    db_session.global_init("server.db")
+    app.run(host='127.0.0.1', port=80)
 
 
 if __name__ == "__main__":
