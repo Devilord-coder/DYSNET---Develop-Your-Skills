4072ad28bf70174cdc6d7cfc0542a3f4352cced5
Danya Putilin <danyaputilin@yandex.ru>
2026-04-03 23:58:45 +0300

    profile & python_api update

---
+++ backend/forms/__init__.py
@@ -0,0 +1,11 @@
+from backend.forms.login_form import LoginForm
+from backend.forms.register_form import RegisterForm
+from backend.forms.edit_profile_form import EditProfileForm
+
+"""Все формы"""
+
+__all__ = [
+    "LoginForm",
+    "RegisterForm",
+    "EditProfileForm"
+]
